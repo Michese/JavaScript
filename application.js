5975a855f68a27728bf783461e074544470df453
Michese <michese48@mail.ru>
2020-03-27 12:44:43 +0300

    task 5

--- application.js
+++ application.js
@@ -44,47 +44,80 @@
 // }
 
 // Задание 4
-/**
- * Функция возращает сумму двух чисел
- * @param {number} a первый операнд
- * @param {number} b второй операнд
- * @returns {number}
- */
-function summ(a, b) {
-    return a + b;
-}
+
+// /**
+//  * Функция возращает сумму двух чисел
+//  * @param {number} a первый операнд
+//  * @param {number} b второй операнд
+//  * @returns {number}
+//  */
+// function summ(a, b) {
+//     return a + b;
+// }
+//
+// /**
+//  * Функция возращает разность двух чисел
+//  * @param {number} a первый операнд
+//  * @param {number} b второй операнд
+//  * @returns {number}
+//  */
+// function diff(a, b) {
+//     return a - b;
+// }
+//
+// /**
+//  * Функция возращает произведение двух чисел
+//  * @param {number} a первый операнд
+//  * @param {number} b второй операнд
+//  * @returns {number}
+//  */
+// function comp(a, b) {
+//     return a * b;
+// }
+//
+// /**
+//  * Функция возращает деление двух чисел
+//  * @param {number} a первый операнд
+//  * @param {number} b второй операнд
+//  * @returns {number}
+//  */
+// function div(a, b) {
+//     return a / b;
+// }
+// let a = 5, b = 2;
+// console.log(summ(a, b));
+// console.log(diff(a, b));
+// console.log(comp(a, b));
+// console.log(div(a, b));
+
+// Задание 5
 
 /**
- * Функция возращает разность двух чисел
- * @param {number} a первый операнд
- * @param {number} b второй операнд
+ * Функция возращает результат одной из
+ * четырех бинарных операций: суммы, разности, умножения, деления.
+ * @param {number} arg1 первый операнд
+ * @param {number} arg2 второй операнд
+ * @param {string} operation наименование операции
  * @returns {number}
  */
-function diff(a, b) {
-    return a - b;
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+        case "summ":
+            return arg1 + arg2;
+        case "diff":
+            return arg1 - arg2;
+        case "comp":
+            return arg1 * arg2;
+        case "div":
+            return arg1 / arg2;
+        default:
+            throw new UserException("Неверно указано название операции");
+    }
 }
 
-/**
- * Функция возращает произведение двух чисел
- * @param {number} a первый операнд
- * @param {number} b второй операнд
- * @returns {number}
- */
-function comp(a, b) {
-    return a * b;
-}
-
-/**
- * Функция возращает деление двух чисел
- * @param {number} a первый операнд
- * @param {number} b второй операнд
- * @returns {number}
- */
-function div(a, b) {
-    return a / b;
-}
-let a = 5, b = 2;
-console.log(summ(a, b));
-console.log(diff(a, b));
-console.log(comp(a, b));
-console.log(div(a, b));
+let a = 4, b = 1;
+console.log(mathOperation(a,b,"summ"));
+console.log(mathOperation(a,b,"diff"));
+console.log(mathOperation(a,b,"comp"));
+console.log(mathOperation(a,b,"div"));
+console.log(mathOperation(a,b,"abracadabra"));
