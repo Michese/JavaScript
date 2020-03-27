79240b58ec217573ef661c70941c7396beb55ca4
Michese <michese48@mail.ru>
2020-03-27 12:28:10 +0300

    task 4

--- application.js
+++ application.js
@@ -34,11 +34,57 @@
 
 // Задание 3
 
-let a = 2, b = 3;
-if (a >= 0 && b >= 0) {
-    console.log(a - b);
-} else if ((a >= 0 && b < 0) || (a < 0 && b >= 0)) {
-    console.log(a + b);
-} else {
-    console.log(a * b);
+// let a = 2, b = 3;
+// if (a >= 0 && b >= 0) {
+//     console.log(a - b);
+// } else if ((a >= 0 && b < 0) || (a < 0 && b >= 0)) {
+//     console.log(a + b);
+// } else {
+//     console.log(a * b);
+// }
+
+// Задание 4
+/**
+ * Функция возращает сумму двух чисел
+ * @param {number} a первый операнд
+ * @param {number} b второй операнд
+ * @returns {number}
+ */
+function summ(a, b) {
+    return a + b;
 }
+
+/**
+ * Функция возращает разность двух чисел
+ * @param {number} a первый операнд
+ * @param {number} b второй операнд
+ * @returns {number}
+ */
+function diff(a, b) {
+    return a - b;
+}
+
+/**
+ * Функция возращает произведение двух чисел
+ * @param {number} a первый операнд
+ * @param {number} b второй операнд
+ * @returns {number}
+ */
+function comp(a, b) {
+    return a * b;
+}
+
+/**
+ * Функция возращает деление двух чисел
+ * @param {number} a первый операнд
+ * @param {number} b второй операнд
+ * @returns {number}
+ */
+function div(a, b) {
+    return a / b;
+}
+let a = 5, b = 2;
+console.log(summ(a, b));
+console.log(diff(a, b));
+console.log(comp(a, b));
+console.log(div(a, b));
